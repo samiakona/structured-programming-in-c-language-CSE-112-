4dda59837e813c6a849d6843203c4e2fc49771dc
samiakona <69978344+samiakona@users.noreply.github.com>
2020-08-21 18:15:33 +0600

    programing

---
+++ 2/sub2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>//header file
+/*This program is
+printing substration result*/
+int main()
+{
+    int a=23,b=45;
+    int sub=abs(a-b);
+    printf("sub=%d",sub);
+    return 0;
+}
